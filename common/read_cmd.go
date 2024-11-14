9095166e20efee002496408642d9efd6e1a78569
Brandon lz <advance_to@hotmail.com>
2024-11-14 09:27:08 +0000

    modified:   common/read_cmd.go

--- common/read_cmd.go
+++ common/read_cmd.go
@@ -6,17 +6,18 @@ import (
 	"net"
 )
 
-func ReadCmd(conn net.Conn) ([]byte, error) {          // 这种会丢数据，还是需要用面向对象编程
+func ReadCmd(conn net.Conn) ([]byte, error) { // 这种会丢数据，还是需要用面向对象编程
 	buf := bytes.Buffer{}
 	rd := bufio.NewReader(conn)
 	for {
 		d, err := rd.ReadBytes('\n')
 		if err != nil {
+			return nil, err
 		}
 		buf.Write(d)
 		l := len(buf.Bytes())
 		if l > 1 && buf.Bytes()[l-2] == '\r' {
-			return buf.Bytes()[:l-2],nil
-		} 
+			return buf.Bytes()[:l-2], nil
+		}
 	}
 }
